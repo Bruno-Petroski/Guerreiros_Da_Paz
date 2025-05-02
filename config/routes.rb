c88b4a41b3f889911036cf07f51688a1289353eb
Livia <livia_zimermann@edu.univali.br>
2025-05-02 11:19:38 -0300

    Inclui nas rotas recursos para as tbelas

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,12 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  resources :responsaveis
+  resources :alunos
+  resources :professores
+  resources :turmas do
+  resources :matriculas, only: [:new, :create, :destroy]
+end
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  root to: 'users#index'
 end
