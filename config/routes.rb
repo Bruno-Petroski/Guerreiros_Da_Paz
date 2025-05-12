01bcd7d85b2a6915429ceb2e34e1549a1f38b76f
Livia <livia_zimermann@edu.univali.br>
2025-05-12 20:07:29 -0300

    Fix views and adjust login

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, skip: [:registrations]
+  
+  devise_scope :user do
+    get '/users/sign_in' => 'devise/sessions#new'
+    post '/users/sign_in' => 'devise/sessions#create'
+    delete '/users/sign_out' => 'devise/sessions#destroy'
+  end
 
   resources :users, except: [:show]
 
